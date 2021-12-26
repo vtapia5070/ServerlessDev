2c60077938a22d73edc84e1b97231d1902f323ff
victoria tapia <tapia.victoria5070@gmail.com>
2021-12-26 13:03:04 -0800

    utilise common middleware

---
+++ src/handlers/lib/commonMiddleware.js
@@ -0,0 +1,12 @@
+import httpJsonBodyParser from '@middy/http-json-body-parser';
+import httpEventNormalizer from '@middy/http-event-normalizer';
+import httpErrorHandler from '@middy/http-error-handler';
+import middy from '@middy/core';
+
+
+export default handler => middy(handler)
+  .use([
+    httpJsonBodyParser(),
+    httpEventNormalizer(),
+    httpErrorHandler()
+  ])
