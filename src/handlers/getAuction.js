ab8e63b43a909d4c0d4f054c6289d05f5ee5e7a0
victoria tapia <tapia.victoria5070@gmail.com>
2021-12-26 12:56:36 -0800

    add functions

--- src/handlers/getAuction.js
+++ src/handlers/getAuction.js
@@ -1,4 +1,3 @@
-import { v4 as uuid } from 'uuid';
 import AWS from 'aws-sdk';
 import middy from '@middy/core';
 import httpJsonBodyParser from '@middy/http-json-body-parser';
@@ -8,34 +7,33 @@ import createError from 'http-errors';
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
-async function createAuction(event, context) {
-  const { title } = event.body;
-  const now = new Date();
-
-  const auction = {
-    id: uuid(),
-    title,
-    status: 'OPEN',
-    createdAt: now.toISOString()
-  };
+async function getAuction(event, context) {
+  let auction;
+  const { id } = event.pathParameters
 
   try {
-    await dynamodb.put({
+    const result = await dynamodb.get({
       TableName: process.env.AUCTIONS_TABLE_NAME,
-      Item: auction,
-    }).promise();
-  } catch(error) {
+      Key: { id }
+    }).promise()
+
+    auction = result.Item;
+  } catch (error) {
     console.error(error);
-    throw new createError.InternalServerError(error);
+    throw new createError.InternalServerError(error); // only for local debugging, for prod have status code
+  }
+
+  if (!auction) {
+    throw new createError.NotFount(`Auction with ID "${id}" not found`);
   }
 
   return {
-    statusCode: 201,
+    statusCode: 200,
     body: JSON.stringify(auction),
   };
 }
 
-export const handler = middy(createAuction)
+export const handler = middy(getAuction)
   .use(httpJsonBodyParser())
   .use(httpEventNormalizer())
   .use(httpErrorHandler());
