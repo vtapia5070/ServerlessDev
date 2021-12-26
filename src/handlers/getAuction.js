8a3d9aea3b068953cdeacd5c0b44e3d0ee8f9f7a
victoria tapia <tapia.victoria5070@gmail.com>
2021-12-26 13:33:13 -0800

    add validation, reuse getAuction

--- src/handlers/getAuction.js
+++ src/handlers/getAuction.js
@@ -4,9 +4,8 @@ import commonMiddleware from '../lib/commonMiddleware';
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
-async function getAuction(event, context) {
+export async function getAuctionById (id) {
   let auction;
-  const { id } = event.pathParameters
 
   try {
     const result = await dynamodb.get({
@@ -21,8 +20,16 @@ async function getAuction(event, context) {
   }
 
   if (!auction) {
-    throw new createError.NotFount(`Auction with ID "${id}" not found`);
+    throw new createError.NotFound(`Auction with ID "${id}" not found`);
   }
+
+  return auction;
+}
+
+async function getAuction(event, context) {
+  const { id } = event.pathParameters;
+  const auction = await getAuctionById(id);
+
 
   return {
     statusCode: 200,
