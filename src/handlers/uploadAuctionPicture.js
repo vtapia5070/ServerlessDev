5fd93b5a63a7a14a15f369602894f1a946a8cd82
victoria tapia <tapia.victoria5070@gmail.com>
2022-01-09 16:28:35 +0100

    update auction with picture and error handling

--- src/handlers/uploadAuctionPicture.js
+++ src/handlers/uploadAuctionPicture.js
@@ -3,16 +3,26 @@ import httpErrorHandler from '@middy/http-error-handler';
 import createError from 'http-errors';
 import { getAuctionById } from './getAuction';
 import { uploadPictureToS3 } from '../lib/uploadPictureToS3';
+import { setAuctionPictureUrl } from '../lib/setAuctionPictureUrl';
 
 export async function uploadAuctionPicture (event) {
   const { id } = event.pathParameters;
   const auction = await getAuctionById(id);
+  const { email } = event.requestContext.authorizer;
+
+  if (auction.seller !== email) {
+    throw new createError.Forbidden('You are not the seller of this auction');
+  }
+
   const base64 = event.body.replace(/^data:image\/w+;base64,/, '');
   const buffer = Buffer.from(base64, 'base64');
 
+  let updatedAuction;
+
   try {
-    const uploadToS3Result = await uploadPictureToS3(auction.id + '.jpg', buffer)
-    console.log('uploadToS3Result:', uploadToS3Result)
+    const pictureUrl = await uploadPictureToS3(auction.id + '.jpg', buffer)
+    updatedAuction = await setAuctionPictureUrl(auction.id, pictureUrl);
+
   } catch (error) {
     console.error(error);
     throw new createError.InternalServerError(error);
@@ -20,9 +30,9 @@ export async function uploadAuctionPicture (event) {
 
   return {
     statusCode: 200,
-    body: JSON.stringify({}),
+    body: JSON.stringify(updatedAuction),
   };
 }
 
 export const handler = middy(uploadAuctionPicture)
-  .use(httpErrorHandler);
+  .use(httpErrorHandler());
